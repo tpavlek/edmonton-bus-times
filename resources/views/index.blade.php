f70dbee98dfa7900566d936068da01391dd28270
Troy Pavlek <troy@depotwarehouse.net>
2016-10-26 19:47:23 -0600

    It has a form

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,22 @@
 
 </head>
 <body>
+<form action="/" method="get">
+    <label for="early">
+        Early threshold (seconds):
+        <input type="number" name="early" value="{{ Request::get('early', 180) }}" />
+    </label>
+
+    <label for="late">
+        Late Threshold (seconds):
+        <input type="number" name="late" value="{{ Request::get('late', 180) }}" />
+    </label>
+
+    <input type="submit" value="Go" />
+
+
+</form>
+
 <h1>
     Early: {{ $view['early']->count() }}
 </h1>
